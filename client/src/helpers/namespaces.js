81ab0c79cd4ccd60991cb53ca9ef902174d35ec9
Andrew MacDonald <mac087@gmail.com>
2019-05-13 12:53:21 -0700

    Intialize namespaces

--- client/src/helpers/namespaces.js
+++ client/src/helpers/namespaces.js
@@ -8,19 +8,19 @@ const asteroidIcon = require('../assets/asteroid.svg');
 
 // Set up the namespaces
 const namespaces = [];
-let astronaut = new Namespace(0, 'Astronaut', 'https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/103px-Wikipedia-logo-v2.svg.png', '/astronaut');
-let mozNs = new Namespace(1, 'Mozilla', 'https://www.mozilla.org/media/img/logos/firefox/logo-quantum.9c5e96634f92.png', '/mozilla');
+let astronaut = new Namespace(0, 'Astronaut', astronautIcon, '/astronaut');
+let asteroid = new Namespace(1, 'Asteroid', asteroidIcon, '/asteroid');
 
-namespaces.push(astronaut, mozNs);
+namespaces.push(astronaut, asteroid);
 
 // Make the main room and add it to rooms. it will ALWAYS be 0
-astronaut.addRoom(new Room(0, 'New Articles', 'Wiki'));
-astronaut.addRoom(new Room(1, 'Editors', 'Wiki'));
-astronaut.addRoom(new Room(2, 'Other', 'Wiki'));
+astronaut.addRoom(new Room(0, 'General', 'Astronaut'));
+astronaut.addRoom(new Room(1, 'Events', 'Astronaut'));
+astronaut.addRoom(new Room(2, 'Random', 'Astronaut'));
 
-mozNs.addRoom(new Room(0, 'Firefox', 'Mozilla'));
-mozNs.addRoom(new Room(1, 'SeaMonkey', 'Mozilla'));
-mozNs.addRoom(new Room(2, 'SpiderMonkey', 'Mozilla'));
-mozNs.addRoom(new Room(3, 'Rust', 'Mozilla'));
+asteroid.addRoom(new Room(0, 'General', 'Asteroid'));
+asteroid.addRoom(new Room(1, 'Helpful', 'Asteroid'));
+asteroid.addRoom(new Room(2, 'Chatter', 'Asteroid'));
+asteroid.addRoom(new Room(3, 'Random', 'Asteroid'));
 
 module.exports = namespaces;
